d532a5210468d4d79d93b8539822a238d2e1da02
fforw@gmx.de <fforw@gmx.de@b28efbb6-974e-0410-a7d0-456cb01d9dc4>
2009-03-30 22:26:58 +0000

    retag release 0.9.0-1

---
+++ trunk/src/org/jcouchdb/db/Response.java
@@ -0,0 +1,175 @@
+package org.jcouchdb.db;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.httpclient.Header;
+import org.apache.log4j.Logger;
+import org.jcouchdb.document.InstanceCachable;
+import org.svenson.JSONParser;
+
+/**
+ * Encapsulates a couchdb server response with error code and received
+ * body.
+ *
+ * @author shelmberger
+ *
+ */
+public class Response
+{
+    protected static Logger log = Logger.getLogger(Response.class);
+
+    private int code;
+    private byte[] content;
+
+    private JSONParser parser;
+
+    private Map<Class,Object> responseCache = new HashMap<Class,Object>();
+
+    private String stringContent;
+
+    private Header[] headers;
+
+    public Response(int code, byte[] content)
+    {
+        this(code,content,null);
+    }
+
+    public Response(int code, String content)
+    {
+        this(code,content,null);
+    }
+
+    public Response(int code, byte[] content, Header[] headers)
+    {
+        this.code = code;
+        this.content = content;
+        this.headers = headers;
+
+        if (log.isDebugEnabled())
+        {
+            log.debug(this);
+        }
+    }
+
+    public Response(int code, String content, Header[] headers)
+    {
+        this.code = code;
+        this.content = null;
+        this.stringContent = content;
+        this.headers = headers;
+
+        if (log.isDebugEnabled())
+        {
+            log.debug(this);
+        }
+    }
+
+    public void setParser(JSONParser parser)
+    {
+        this.parser = parser;
+    }
+
+    private JSONParser getParser()
+    {
+        if (parser == null)
+        {
+            parser = new JSONParser();
+        }
+        return parser;
+    }
+
+    public int getCode()
+    {
+        return code;
+    }
+
+    public byte[] getContent()
+    {
+        if (content == null)
+        {
+            content = stringContent.getBytes();
+        }
+
+        return content;
+    }
+
+    public String getContentAsString()
+    {
+        if (stringContent == null)
+        {
+            stringContent = new String(content);
+            content = null;
+        }
+        return stringContent;
+    }
+
+    /**
+     * Returns the contents of the response as List
+     * @return
+     */
+    public List getContentAsList()
+    {
+        return getParser().parse(List.class, getContentAsString());
+    }
+
+    /**
+     * Returns the contents of the response as Map
+     * @return
+     */
+    public Map getContentAsMap()
+    {
+        return getParser().parse(Map.class, getContentAsString());
+    }
+
+    /**
+     * Returns the contents of the response as bean of the
+     * given type.
+     *
+     * @return
+     */
+    public <T> T getContentAsBean(Class<T> cls)
+    {
+        T cached = null;
+
+        boolean instanceCachable = cls.getAnnotation(InstanceCachable.class) != null;
+
+        if (instanceCachable)
+        {
+            cached = (T)responseCache.get(cls);
+        }
+        if (cached == null)
+        {
+            cached = getParser().parse(cls, getContentAsString());
+
+            if (instanceCachable)
+            {
+                responseCache.put(cls, cached);
+            }
+        }
+        return cached;
+    }
+
+    public Header[] getResponseHeaders()
+    {
+        return headers;
+    }
+
+    /**
+     * Returns <code>true</code> if the response code is
+     * between 200 and 299
+     * @return
+     */
+    public boolean isOk()
+    {
+        return code >= 200 && code <= 299;
+    }
+
+    @Override
+    public String toString()
+    {
+        return super.toString()+": code = "+code+", content = "+( stringContent != null ? stringContent : new String(content));
+    }
+}
+
---
+++ trunk/src/org/jcouchdb/db/ServerImpl.java
@@ -0,0 +1,333 @@
+package org.jcouchdb.db;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.HostConfiguration;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+import org.apache.commons.httpclient.NoHttpResponseException;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.ByteArrayRequestEntity;
+import org.apache.commons.httpclient.methods.DeleteMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.PutMethod;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.apache.log4j.Logger;
+import org.jcouchdb.exception.CouchDBException;
+import org.jcouchdb.exception.NoResponseException;
+import org.jcouchdb.util.ExceptionWrapper;
+
+/**
+ * Default implementation of the {@link Server} interface.
+ * 
+ * @author shelmberger
+ */
+public class ServerImpl
+    implements Server
+{
+    private static final String CHARSET = "UTF-8";
+
+    protected static Logger log = Logger.getLogger(ServerImpl.class);
+
+    private HostConfiguration hostConfiguration;
+
+    private HttpClient httpClient = new HttpClient();
+
+    public ServerImpl(String host)
+    {
+        this(host, DEFAULT_PORT);
+    }
+
+    public ServerImpl(String host, int port)
+    {
+        this.hostConfiguration = new HostConfiguration();
+        this.hostConfiguration.setHost(host, port);
+        httpClient.setHostConfiguration(hostConfiguration);
+        httpClient.setHttpConnectionManager(new MultiThreadedHttpConnectionManager());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<String> listDatabases()
+    {
+        Response resp = get("/_all_dbs");
+        if (!resp.isOk())
+        {
+            throw new CouchDBException("Error listing databases: " + resp);
+        }
+        return resp.getContentAsList();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean createDatabase(String name)
+    {
+        Response resp = put("/" + name + "/");
+        if (resp.isOk())
+        {
+            return true;
+        }
+        else
+        {
+            if (resp.getCode() == 412 || resp.getCode() == 500)
+            {
+                return false;
+            }
+            else
+            {
+                throw new CouchDBException("Error creating database: " + resp);
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void deleteDatabase(String name)
+    {
+        Response resp = delete("/" + name + "/");
+        if (!resp.isOk())
+        {
+            throw new CouchDBException("Cannot delete database " + name + ": " + resp);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Response get(String uri)
+    {
+        if (log.isDebugEnabled())
+        {
+            log.debug("GET " + uri);
+        }
+
+        GetMethod method = new GetMethod(uri);
+
+        try
+        {
+            int code = httpClient.executeMethod(method);
+            Response response = new Response(code, method.getResponseBody(), method
+                .getResponseHeaders());
+            return response;
+        }
+        catch (NoHttpResponseException e)
+        {
+            throw new NoResponseException();
+        }
+        catch (HttpException e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        catch (IOException e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        catch (Exception e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        finally
+        {
+            method.releaseConnection();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Response put(String uri)
+    {
+        return put(uri, null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Response put(String uri, String body)
+    {
+        if (log.isDebugEnabled())
+        {
+            log.debug("PUT " + uri + ", body = " + body);
+        }
+        PutMethod putMethod = new PutMethod(uri);
+
+        try
+        {
+            if (body != null)
+            {
+                putMethod.setRequestEntity(new StringRequestEntity(body, "application/json",
+                    CHARSET));
+            }
+            int code = httpClient.executeMethod(putMethod);
+            Response response = new Response(code, putMethod.getResponseBody(), putMethod
+                .getResponseHeaders());
+            return response;
+        }
+        catch (NoHttpResponseException e)
+        {
+            throw new NoResponseException();
+        }
+        catch (HttpException e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        catch (IOException e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        catch (Exception e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        finally
+        {
+            putMethod.releaseConnection();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Response put(String uri, byte[] body, String contentType)
+    {
+        if (log.isDebugEnabled())
+        {
+            log.debug("PUT " + uri + ", body = " + body);
+        }
+
+        PutMethod putMethod = new PutMethod(uri);
+        if (body != null)
+        {
+            putMethod.setRequestEntity(new ByteArrayRequestEntity(body, contentType));
+        }
+
+        try
+        {
+            int code = httpClient.executeMethod(putMethod);
+            Response response = new Response(code, putMethod.getResponseBody(), putMethod
+                .getResponseHeaders());
+
+            return response;
+        }
+        catch (NoHttpResponseException e)
+        {
+            throw new NoResponseException();
+        }
+        catch (HttpException e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        catch (IOException e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        catch (Exception e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        finally
+        {
+            putMethod.releaseConnection();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Response post(String uri, String body)
+    {
+        if (log.isDebugEnabled())
+        {
+            log.debug("POST " + uri + ", body = " + body);
+        }
+
+        PostMethod postMethod = new PostMethod(uri);
+
+        try
+        {
+            postMethod.setRequestEntity(new StringRequestEntity(body, "application/json", CHARSET));
+            int code = httpClient.executeMethod(postMethod);
+            Response response = new Response(code, postMethod.getResponseBody(), postMethod
+                .getResponseHeaders());
+
+            return response;
+        }
+        catch (NoHttpResponseException e)
+        {
+            throw new NoResponseException();
+        }
+        catch (HttpException e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        catch (IOException e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        catch (Exception e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        finally
+        {
+            postMethod.releaseConnection();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Response delete(String uri)
+    {
+        if (log.isDebugEnabled())
+        {
+            log.debug("DELETE " + uri);
+        }
+
+        DeleteMethod deleteMethod = new DeleteMethod(uri);
+
+        try
+        {
+            int code = httpClient.executeMethod(deleteMethod);
+            Response response = new Response(code, deleteMethod.getResponseBody(), deleteMethod
+                .getResponseHeaders());
+
+            return response;
+        }
+        catch (NoHttpResponseException e)
+        {
+            throw new NoResponseException();
+        }
+        catch (HttpException e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        catch (IOException e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        catch (Exception e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        finally
+        {
+            deleteMethod.releaseConnection();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void setCredentials(AuthScope authScope, Credentials credentials)
+    {
+        httpClient.getState().setCredentials(authScope, credentials);
+    }
+}
