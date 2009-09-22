c83bd59d16fb8e61b1a431e097ac2ef57801ce45
fforw@gmx.de <fforw@gmx.de@b28efbb6-974e-0410-a7d0-456cb01d9dc4>
2009-09-22 20:40:57 +0000

    prepare branch for couchdb 0.10.x support

---
+++ branches/0.10.x/test/org/jcouchdb/db/OptionsTestCase.java
@@ -0,0 +1,38 @@
+package org.jcouchdb.db;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.startsWith;
+
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+
+public class OptionsTestCase
+{
+    protected static Logger log = Logger.getLogger(OptionsTestCase.class);
+
+    @Test
+    public void option()
+    {
+        String query = new Options("foo",1).put("bar", "baz!").toQuery();
+        log.debug(query);
+        assertThat(query, startsWith("?"));
+        assertThat(query, containsString("foo=1"));
+        assertThat(query, containsString("bar=%22baz%21%22"));
+
+
+        query = new Options("foo",1).put("bar", new ArrayList()).toQuery();
+        log.debug(query);
+        assertThat(query, startsWith("?"));
+        assertThat(query, containsString("foo=1"));
+        assertThat(query, containsString("bar=%5B%5D"));
+
+        query = new Options().startKeyDocId("bar").toQuery();
+        log.debug(query);
+        assertThat(query, startsWith("?"));
+        assertThat(query, containsString("startkey_docid=bar"));
+    }
+}
