da293c7c3f5d22b8c9ac6bb0d3d9b745a97fb064
fforw@gmx.de <fforw@gmx.de@b28efbb6-974e-0410-a7d0-456cb01d9dc4>
2009-10-09 13:12:54 +0000

    added resource sync util, media type util

---
+++ src/org/jcouchdb/util/ResourceSync.java
@@ -0,0 +1,102 @@
+package org.jcouchdb.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Collection;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.jcouchdb.db.Database;
+import org.jcouchdb.document.BaseDocument;
+import org.jcouchdb.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ResourceSync
+{
+    private static Logger log = LoggerFactory.getLogger(ResourceSync.class);
+    
+    private Database database;
+
+    private File resourceBaseDir;
+
+    private String resourceBaseDocId = "res";
+    
+    private MediaTypeUtil mediaTypeUtil = new MediaTypeUtil();
+    
+    
+    public void setResourceBaseDocId(String resourceBaseDocId)
+    {
+        this.resourceBaseDocId = resourceBaseDocId;
+    }
+    
+    public String getResourceBaseDocId()
+    {
+        return resourceBaseDocId;
+    }
+
+
+    public void setDatabase(Database database)
+    {
+        this.database = database;
+    }
+
+
+    public void setResourceBaseDir(File resourceBaseDir)
+    {
+        Assert.notNull(resourceBaseDir, "resourceBaseDir can't be null");
+
+        if (resourceBaseDir.exists())
+        {
+            Assert.isTrue(resourceBaseDir.isDirectory(), resourceBaseDir + " is not a directory");
+        }
+        else
+        {
+            resourceBaseDir.mkdirs();
+        }
+
+        this.resourceBaseDir = resourceBaseDir;
+    }
+
+
+    public void syncResources()
+    {
+        String revision = null; 
+        try
+        {
+            BaseDocument doc = database.getDocument(BaseDocument.class, resourceBaseDocId);
+            revision = doc.getRevision();
+            
+        }
+        catch(NotFoundException e)
+        {
+            // revision stays null for "no document";
+        }
+
+        String basePath = resourceBaseDir.getPath();
+        
+        for (File f : (Collection<File>)FileUtils.listFiles(resourceBaseDir, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE))
+        {
+            String path = f.getPath();
+            if (!path.startsWith(basePath))
+            {
+                throw new IllegalStateException(f + "'s path does not start with '" + basePath + "'");
+            }
+            
+            String attachmentId = path.substring(basePath.length() + 1);
+            
+            log.debug("found file {} => attachment id = {}", f,attachmentId);
+            
+            String mediaType = mediaTypeUtil.getMediaTypeForName(attachmentId);
+            try
+            {
+                revision = database.createAttachment(resourceBaseDocId, revision, attachmentId, mediaType, new FileInputStream(f), f.length());
+            }
+            catch (FileNotFoundException e)
+            {
+                throw ExceptionWrapper.wrap(e);
+            }
+        }
+    }
+}
